7e083b5548722dac50cff9047999e7bb0f929742
GerminaTech <98488330+germinatech@users.noreply.github.com>
2022-08-26 15:38:05 -0300

    Update script.js

--- EX2DOM/script.js
+++ EX2DOM/script.js
@@ -1,22 +1,3 @@
-// O aluno deve sortear um bilhete da mega sena, e comparar com os numeros sorteados presentes no HTML (unico arquivo fornecido a ele)
-// para bilhetes iguais sorteados, informar que deve ocorrer novamente
+// O aluno deve sortear um bilhete da mega sena, e comparar com os numeros sorteados presentes no HTML (unico arquivo fornecido com conteúdo)
+// para bilhetes iguais sorteados, informar que deve ocorrer novamente o sorteio
 // para bilhetes válidos, deve-se contar o numero de acertos e apresentar ao usuario via console
-
-// sorteando um bilhete
-let num1, num2, num3, num4, num5, num6;
-
-num1 = Math.ceil(Math.random() * 60);
-num2 = Math.ceil(Math.random() * 60);
-num3 = Math.ceil(Math.random() * 60);
-num4 = Math.ceil(Math.random() * 60);
-num5 = Math.ceil(Math.random() * 60);
-num6 = Math.ceil(Math.random() * 60);
-
-console.log(num1);
-console.log(num2);
-console.log(num3);
-console.log(num4);
-console.log(num5);
-console.log(num6);
-
-// verificando se o bilhete e valido
